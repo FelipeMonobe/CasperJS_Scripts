1dd3df807a210538effd055983f98e863760f5f5
FelipeMonobe <xinube@gmail.com>
2016-03-23 12:08:45 -0300

    facebook login and msg

--- facebookLoginAndMsg.js
+++ facebookLoginAndMsg.js
@@ -4,16 +4,16 @@ var credentials = require('./credentials.json');
 var receipt = casper.cli.args[0],
   message = casper.cli.args[1];
 
-  if (!receipt || !message) {
-    var missingArgs = receipt ? 'message' :
-      message ? 'receipt' :
-      'message and receipt';
+if (!receipt || !message) {
+  var missingArgs = receipt ? 'message' :
+    message ? 'receipt' :
+    'message and receipt';
 
-    casper.echo('ERROR: missing arguments ' + missingArgs);
-    casper.exit();
-  }
+  casper.echo('ERROR: missing arguments ' + missingArgs);
+  casper.exit();
+}
 
-casper.start('https://www.facebook.com/messages/' + receipt, function() {
+casper.start('https://www.facebook.com/messages/new', function() {
   casper.echo('1 - page loaded');
 });
 
@@ -27,9 +27,18 @@ casper.then(function() {
 
 casper.then(function() {
   casper.waitForText('Reply', function() {
-    casper.echo('3 - Sending message');
+    casper.echo('3 - Searching receipt');
+    casper.sendKeys('input[data-placeholder="Name"]', receipt, {
+      keepFocus: true
+    });
+    casper.sendKeys('input[data-placeholder="Name"]', casper.page.event.key.Enter);
+
+    casper.echo('4 - Sending message');
     casper.sendKeys('.uiTextareaNoResize', message);
-    casper.click('input[value="Reply"]');
+    casper.sendKeys('.uiTextareaNoResize', casper.page.event.key.Enter);
+    casper.sendKeys('.uiTextareaNoResize', casper.page.event.key.Enter, {
+      modifiers: 'ctrl'
+    });
   }, 30000);
 });
 
