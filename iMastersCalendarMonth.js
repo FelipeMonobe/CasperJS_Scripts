5914efce6e9b9ba914d2a1311e550a6e38926000
FelipeMonobe <xinube@gmail.com>
2016-03-28 16:01:36 -0300

    WIP

--- iMastersCalendarMonth.js
+++ iMastersCalendarMonth.js
@@ -1,61 +1,56 @@
 var casper = require('casper').create();
 var events = [];
 var futureEvents = [];
-var today = new Date().getDate();
+var today = new Date();
 var eventList = [];
+var hasNext = true;
 
-debugger;
+var day = today.getDate();
+var month = today.getMonth()+1;
+var year = today.getFullYear();
+month = month < 10 ? '0' + month : month;
+var page = 1;
+var baseUrl = 'http://imasters.com.br/agenda/' + year + '/' + month;
+var url = '';
+var datePattern = /[0-9/]+/;
 
-casper.start('http://imasters.com.br/agenda/', function() {
+do {
+
+url = baseUrl + '/?_paged=' + page
+casper.echo('Acessing ' + url);
+
+casper.start(url, function() {
   casper.echo('1 - page loaded');
 });
 
-casper.waitForSelector('.has', function() {
-  casper.echo('2 - checking days');
-  events = casper.evaluate(function() {
-    return Array.prototype.slice.call(document.querySelectorAll('.has>a'));
-  });
+casper.waitForSelector('.location', function() {
+  casper.echo('2 - checking events');
+  eventList.push(casper.evaluate(function() {
+    var stringNA = 'Não disponível',
+      calendarEvents = [],
+      containers = Array.prototype.slice.call(document.querySelectorAll('section.published'));
 
-  casper.echo('events');
-  casper.echo(events);
+    containers.forEach(function(container) {
+      var qsA = container.querySelector('a'),
+        qsDate = container.querySelector('.date'),
+        qsLocation = container.querySelector('.location'),
+	qsH3 = container.querySelector('h3');
 
-  futureEvents = events.filter(function(evt) {
-    return parseInt(evt.innerText) >= today;
-  });
+      calendarEvents.push({
+        name: qsA ? qsA.title.trim() : qsH3.innerHTML.trim(),
+        date: qsDate ? qsDate.innerHTML.trim() : stringNA,
+        location: qsLocation ? qsLocation.innerHTML.trim() : stringNA,
+        url: qsA ? qsA.href.trim() : stringNA
+      });
+    });
 
-  casper.echo('future events');
-  casper.echo(futureEvents);
-
+    page++;
+    hasNext = document.querySelectorAll('a.next').length > 0;
+    return calendarEvents;
+  }));
 }, null, 30000);
 
-/*
-futureEvents.forEach(function(fEvent) {
-  casper.thenOpen(fEvent.href, function() {
-    casper.waitForSelector('.location', function() {
-      eventList = eventList.concat(casper.evaluate(function() {
-        var stringNA = 'Não disponível',
-          calendarEvents = [],
-          containers = Array.prototype.slice.call(document.querySelectorAll('section.published'));
-
-        containers.forEach(function(container) {
-          var qsA = container.querySelector('a'),
-            qsDate = container.querySelector('.date'),
-            qsLocation = container.querySelector('.location');
-
-	  calendarEvents.push({
-            name: qsA ? qsA.title.trim() : stringNA,
-            date: qsDate ? qsDate.innerHTML.trim() : stringNA,
-            location: qsLocation ? qsLocation.innerHTML.trim() : stringNA,
-            url: qsA ? qsA.href.trim() : stringNA
-          });
-        });
-
-       return calendarEvents;
-    }));
-}, null, 30000);
-  })
-});
-*/
+} while (hasNext)
 
 casper.run(function() {
   casper.echo('4 - printing results\n');
